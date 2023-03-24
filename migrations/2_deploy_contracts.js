e0628582a6d7355a9d3c47a900a5036eaf2d5531
Sem <931684+sembrestels@users.noreply.github.com>
2023-03-24 15:31:34 +0100

    Migrate to hardhat

--- migrations/2_deploy_contracts.js
+++
@@ -1,5 +0,0 @@
-var Surgery = artifacts.require('./Surgery.sol')
-
-module.exports = function (deployer) {
-  deployer.deploy(Surgery)
-}
--- test/app.js
+++ test/app.js
@@ -1,46 +1,30 @@
 /* global artifacts contract beforeEach it assert */
 
-const { assertRevert } = require('@aragon/test-helpers/assertThrow')
-const { getEventArgument } = require('@aragon/test-helpers/events')
-const { hash } = require('eth-ens-namehash')
-const deployDAO = require('./helpers/deployDAO')
+const { injectWeb3, injectArtifacts } = require('@1hive/contract-helpers-test')
+const { newDao, installNewApp } = require('@1hive/contract-helpers-test/src/aragon-os')
+const { isAddress } = require('ethers/lib/utils')
+
+injectWeb3(web3)
+injectArtifacts(artifacts)
 
 const Surgery = artifacts.require('Surgery.sol')
 
-const ANY_ADDRESS = '0xffffffffffffffffffffffffffffffffffffffff'
-
 contract('Surgery', ([appManager, user]) => {
-  let app
+  let surgeryBase, app
+
+  const APP_ID = '0x1234123412341234123412341234123412341234123412341234123412341234'
+
+  before(async () => {
+    // Deploy the app's base contract
+    surgeryBase = await Surgery.new()
+  })
 
   beforeEach('deploy dao and app', async () => {
-    const { dao, acl } = await deployDAO(appManager)
-
-    // Deploy the app's base contract.
-    const appBase = await Surgery.new()
-
-    // Instantiate a proxy for the app, using the base contract as its logic implementation.
-    const instanceReceipt = await dao.newAppInstance(
-      hash('app.aragonpm.test'), // appId - Unique identifier for each app installed in the DAO; can be any bytes32 string in the tests.
-      appBase.address, // appBase - Location of the app's base implementation.
-      '0x', // initializePayload - Used to instantiate and initialize the proxy in the same call (if given a non-empty bytes string).
-      false, // setDefault - Whether the app proxy is the default proxy.
-      { from: appManager }
-    )
-    app = Surgery.at(
-      getEventArgument(instanceReceipt, 'NewAppProxy', 'proxy')
-    )
-
-    // Set up the app's permissions.
-    // await acl.createPermission(
-    //   ANY_ADDRESS, // entity (who?) - The entity or address that will have the permission.
-    //   app.address, // app (where?) - The app that holds the role involved in this permission.
-    //   await app.INCREMENT_ROLE(), // role (what?) - The particular role that the entity is being assigned to in this permission.
-    //   appManager, // manager - Can grant/revoke further permissions for this role.
-    //   { from: appManager }
-    // )
+    const { dao } = await newDao(appManager)
+    app = Surgery.at(await installNewApp(dao, APP_ID, surgeryBase.address, appManager))
   })
 
   it('app should be deployed', async () => {
-    assert.equal(web3.isAddress(app.address), true)
+    assert.isTrue(isAddress((await app).address), 'app should be initialized')
   })
 })
